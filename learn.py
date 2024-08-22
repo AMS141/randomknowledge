296d4e49257629e7f0fbcabe9a0c10043600e353
AMS141 <abeejmuhamedsait@gmail.com>
2024-08-22 23:04:14 +0530

    removed comments

--- learn.py
+++ learn.py
@@ -2,9 +2,6 @@ Num1=int(input("Enter first number: "))
 Num2=int(input("Enter second number: "))
 choice=input("Enter your operand: ")
 
-#user input = + , -, /, *, %
-#Enter your operand: 
-#error message on division with 0
 
 if choice=="+":
     print("Result: ", Num1+Num2)
